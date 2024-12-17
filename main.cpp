2ca7ff232eea24ef96ccd64a3bd7b180221302fe
Quentin2006 <124484154+Quentin2006@users.noreply.github.com>
2024-12-16 19:41:30 -0800

    finaly made it properly render a circle!!

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main() {
     // WIDTH OF THE IMAGE, HEIGHT IS AUTO CALULATED VIA ASPECT RATIO
     int imgWidth {1920};
     double aspectRatio {16.0/9.0};
-    int imgHeight {1080};
+    int imgHeight {int(imgWidth / aspectRatio)};
     double focalLength {1}; // how far away the veiwplane is in the z-axis
 
     // location of camera
@@ -20,7 +20,7 @@ int main() {
 
     // calulates the viewport demention, the plane of which rays will be sent through
     double viewportHeight {2};
-    double viewportWidth {viewportHeight / (imgWidth / imgHeight)};
+    double viewportWidth {viewportHeight * (double(imgWidth) / imgHeight)};
 
     // calulates how spaced out each pixel will be
     double verticalDelta {viewportHeight/imgHeight};
@@ -28,7 +28,7 @@ int main() {
 
     // calculates the top left of the viewport, aswell as where the pixel will lie of this plane
     point3 viewportTopLeft, pixelTopLeft;
-    viewportTopLeft.makePoint(cameraLoc.getX() + viewportWidth/2, cameraLoc.getY() + viewportHeight/2, focalLength);
+    viewportTopLeft.makePoint(cameraLoc.getX() - viewportWidth/2, cameraLoc.getY() + viewportHeight/2, -focalLength);
     // the 0.5 will cener the rau inside the pixel
     pixelTopLeft.makePoint(viewportTopLeft.getX() + 0.5 * horozantalDelta, viewportTopLeft.getY() - 0.5 * verticalDelta, viewportTopLeft.getZ());
 
@@ -40,6 +40,7 @@ int main() {
          << "255" << endl;
 
     for (int j{0}; j < imgHeight; ++j) {
+        clog << "\rScanlines remaning: " << (imgHeight - j) << ' ' << flush;
         for (int i{0}; i < imgWidth; ++i) {
             // will find the point at which the current pixel is at
             point3 pixelCenter;
@@ -57,6 +58,8 @@ int main() {
         }
     }
 
+    clog << "\rDone.                    \n";
+
     return 0;
 }
 
@@ -65,17 +68,17 @@ int main() {
 // the descrimanites, this will tell us the # of intersection
 // if the # of intersection is >= 1, we can draw red
 bool hitSphere(ray & curRay, double radius, point3 & center) {
-    vec3 dir, oc;
+    vec3 oc;
 
     oc.makeVector(
-            curRay.origin().getX() - center.getX(),
-            curRay.origin().getY() - center.getY(),
-            curRay.origin().getZ() - center.getZ()
-        );
+            -curRay.origin().getX() + center.getX(),
+            -curRay.origin().getY() + center.getY(),
+            -curRay.origin().getZ() + center.getZ()
+    );
 
     double a {curRay.direction().dot(curRay.direction())};
-    double b {2.0 * (curRay.direction().dot(oc))};
-    double c {oc.dot(oc) - radius * radius};
+    double b {-2.0 * (curRay.direction().dot(oc))};
+    double c {oc.dot(oc) - radius*radius};
 
     double discriminant {b*b - 4*a*c};
 
@@ -85,17 +88,16 @@ bool hitSphere(ray & curRay, double radius, point3 & center) {
 // will draw the ray color given the passed ray
 void rayColor(ray & curRay) {
     point3 center;
-    center.makePoint(20, 0, 10);
+    center.makePoint(0, 0, 5);
 
-    if (hitSphere(curRay, 1, center)) {
+    if (hitSphere(curRay, 0.5, center)) {
         writeColor(1,0,0);
         return;
     }
 
     vec3 a;
-
     a.makeVector(curRay.direction().getX(), curRay.direction().getY(), curRay.direction().getZ());
-
+    a.normalize();
     // this normaized ray dir is an double [-1, 1]
     // we want a double [0,1], to do this, we can first add 1 to the vector
     // to get a domain of [0, 2], then divide by 2 to get a domain of [0,1]
@@ -103,7 +105,7 @@ void rayColor(ray & curRay) {
     a.scalarMultiply(0.5);
 
     // throwing these values to render a fun image
-    writeColor(1 - a.getX() + a.getX() * 0.3, 
-               1 - a.getY() + a.getY() * 0.5,
+    writeColor(1 - a.getX() + a.getX() * 0.5, 
+               1 - a.getY() + a.getY() * 0.7,
                1 - a.getZ() + a.getZ() * 1.0);
 }
